a149238b63c66695494fc29f2da3a98b10621e2f
Raza-Lab029 <razakimlie200@gmail.com>
2025-06-04 08:43:08 +0800

    Update appwriteConfig.ts

--- appwriteConfig.ts
+++ appwriteConfig.ts
@@ -1,8 +1,8 @@
 import { Client, Databases, Account } from 'appwrite';
 
 const client = new Client()
-  .setEndpoint('https://YOUR_APPWRITE_ENDPOINT')  // Replace with your Appwrite endpoint
-  .setProject('YOUR_PROJECT_ID');                  // Replace with your project ID
+  .setEndpoint('https://YOUR_APPWRITE_ENDPOINT')  
+  .setProject('YOUR_PROJECT_ID');                 
 
 export const databases = new Databases(client);
 export const account = new Account(client);
